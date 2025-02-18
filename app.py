a50f10010b79143406b2ce269e70da5fcc8db0a6
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2025-02-18 14:24:37 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+# snake_case para variaveis, funcoes e metodos
+# PascalCase para classes
+
+import os
+
+def exibir_nome_projeto():
+    print('''
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
+    ''')
+
+def finalizar_app():
+    os.system('cls')
+    print('finalizando sistema...')
+
+def exibir_opcoes():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha a opção: '))
+    match opcao_escolhida:
+        case 1:
+            print('Adicionar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            print('Finalizar app')
+        case _:
+            print('Opção inválida!')
+
+def main():
+    exibir_nome_projeto()
+    exibir_opcoes()
+    escolher_opcao()
+
+# Quando pedimos para que um programa Python seja executado, o interpretador
+# cria uma variável chamada __name__. Se o __name__ for __main__ (principal, em inglês), significa que esse 
+# código não será importado por outros scripts de código Python, e ele será o programa principal.
+if __name__ == '__main__':
+    main()
